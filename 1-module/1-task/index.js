d20f73fcfc3d74ee70fed6225757290e7786afaa
Renat Kasimov <darka9e@gmail.com>
2019-10-10 20:29:52 +0200

    Second task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,21 +4,21 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow(m, n) {
-    let result = m;
+function pow(x, n) {
+    let result = x;
   
     for (let i = 1; i < n; i++) {
-      result *= m;
+      result *= x;
     }
   
     return result;
   }
   
-  let m = prompt("m?", '');
+  let x = prompt("x?", '');
   let n = prompt("n?", '');
   
   if (n < 1) {
     alert(`Степень ${n} не поддерживается, используйте натуральное число`);
   } else {
-    alert( pow(m, n) );
+    alert( pow(x, n) );
   }
